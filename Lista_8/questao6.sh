38d125eeb8a64114cd0798fcf5ae3a0792a8fbd6
Bruno Ravi <bruno.ravi@academico.ifpb.edu.br>
2020-11-04 22:20:34 -0300

    Atualização da questão 6

--- Lista_8/questao6.sh
+++ Lista_8/questao6.sh
@@ -4,17 +4,6 @@ linhas=1
 colunas=$(head -${linhas} $1 | wc -c)
 caractere1=1
 caractere2=10
-#for i in $(cat $1);
-#do
-#	echo $i | cut -c ${caractere1}-${caractere2}
-#	caractere1=$(( caractere1+10 ))
-#	caractere2=$(( caractere2+10 ))
-#	if [ $caractere2 -ge $colunas ]; then
-#		linhas=$((linha+1))
-#	fi
-#	read a
-#done
-
 while true;
 do
 	head -${linhas} $1 | tail -1 | cut -c ${caractere1}-${caractere2}
@@ -25,7 +14,7 @@ do
 		caractere1=1
 		caractere2=10
 	fi
-	if [ $linhas -eq $(cat $1 | wc -l) ]; then
+	if [ $linhas -gt $(cat $1 | wc -l) ]; then
 		break
 	fi
 	read a
